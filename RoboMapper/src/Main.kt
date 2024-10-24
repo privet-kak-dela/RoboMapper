bb14a092f1476f239bb29a76ad4ca7bcb456a72b
BUka228 <128601089+BUka228@users.noreply.github.com>
2024-10-24 22:15:07 +0300

    Скелет карты

--- RoboMapper/src/Main.kt
+++ RoboMapper/src/Main.kt
@@ -1,3 +1,34 @@
+import javafx.application.Application
+import javafx.stage.Stage
+import map.*
+
+
+// Отображение карты
+class Main : Application() {
+
+    override fun start(primaryStage: Stage) {
+        val map = Map() // Создаем экземпляр класса Map
+        map.initMap(20, 20) // Инициализируем карту размером 20x20
+
+        val mapDisplay = MapDisplay(map)
+        mapDisplay.start(primaryStage)
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    /*val map = Map()
+    map.initMap(10, 10)
+
+    // Обновление карты
+    map.updateMap(5, 5, 1) // Пометить ячейку с координатами (5, 5) как препятствие
+
+    // Сохранение карты
+    map.saveMap("my_map.txt")*/
+
+    // Загрузка карты
+    /*val loadedMap = Map()
+    loadedMap.loadMap("my_map.txt")*/
+
+    // Отображение карты
+    Application.launch(Main::class.java)
 }
